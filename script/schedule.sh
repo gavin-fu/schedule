7a4ee66effd35f9c107707354e1378b8b25f9234
gavin <gavinfu@yeah.net>
2019-01-21 20:30:12 +0800

    Update schedule.sh

--- script/schedule.sh
+++ script/schedule.sh
@@ -4,13 +4,14 @@
 #       Usage: nohup ./schedule.sh &> schedule.log
 # Description: ---
 #     Options: ---
-#      Author: gavin, gavinfu@yeah.net
+#      Author: gavin, gavin-fu@outlook.com
 #     Created: 20190118
 # =============================================================================
 # 环境配置
+set -f
 home="$(cd $(dirname $0) 2> /dev/null; pwd)"
-log_home=''
-module_home='${home}/module'
+log_home="${home}/log"
+module_home="${home}/module"
 crontab="${home}/crontab"
 
 # 脚本退出机制
@@ -19,13 +20,11 @@ exits_date=                 # 退出日，默认当天
 exits_hour=                 # 退出时，默认23时
 exits_minute=               # 退出分，默认59分
 
-
-# 并发调度是否存在问题?
 _check_start()
 {
     for (( i=0; i<${#present[@]}; i++)); do
         [[ "${items[i]}" = '*' ]] && continue
-        echo ${items[i]} |grep -wq ${present[i]}
+        echo "${items[i]}" |grep -wq "${present[i]}"
         [ $? -ne 0 ] && return 1
     done
     return 0
@@ -34,14 +33,14 @@ _check_start()
 exits_seconds=$(date -d "${exits_date:-$(date +%F)} ${exits_hour:-23}:${exits_minute:-59}" +%s)
 while true; do
     if [[ "$fix_timed_exit" = 'true' ]]; then
-        [ $(date +%s) -ge $exits_seconds ] break
+        [ $(date +%s) -ge $exits_seconds ] && break
     fi
     
     present=($(date +"%-M %-H %-d %-m %-u")) # Minute Hour Day Month Week
     while read line; do
         {
             items=($line)
-            module="$(echo $line |awk '{print $6}')"
+            module="$(echo ${line} |awk '{print $6}')"
             options="${line#*${module}}"
             
             _check_start
@@ -53,7 +52,7 @@ while true; do
                 printf "$(date +%F\ %H:%M:%S) [INFO] %s\n" "Start ${module} module batch processing..." >> "$log_file"
                 printf "$(date +%F\ %H:%M:%S) [INFO] %s\n" "Command: ${script}" >> "$log_file"
                 
-                $script 2>> "$log_file"
+                $script >> "$log_file" 2>&1
             fi
         } &
     done < <(grep -e '^\s*$' -e '^\s*#' -v "$crontab")
